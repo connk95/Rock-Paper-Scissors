73555e7646bd3772d7184dca555e347e02917eaf
Connor Ketcheson <connorketcheson01@gmail.com>
2022-06-01 10:41:48 +0900

    begin work on computerPlay random function

---
+++ rps.js
@@ -0,0 +1,23 @@
+computerPlay = Math.floor(Math.random() * 3); 
+
+let rps = function(computerPlay) {
+    if (computerPlay == 0) {
+        return "rock";
+    } else if (computerPlay == 1) {
+        return "paper";
+    } else if (computerPlay == 2) {
+        return "scissors";
+    } else {
+        return "something went wrong";
+    }
+    }
+
+    rps();
+
+    
+
+    
+
+
+
+
