9e0960245bbec2302199907a8ba0c69b28387947
Connor Ketcheson <connorketcheson01@gmail.com>
2022-06-03 15:45:05 +0900

    slove issue with incorrect result

--- rps.js
+++ rps.js
@@ -1,77 +1,65 @@
-let select = window.prompt("Please type rock, paper, or scissors.");
-let playerSelection = select.toLowerCase();
+let playerSelection = prompt("Please type rock, paper, or scissors.").toLocaleLowerCase();
 
 console.log("You have selected " + playerSelection + ".");
 
-function computerPlay(rps) {
+function computerPlay() {
     let result;
     let rndInt = Math.floor(Math.random() * 3);
-    if (rndInt == 0) {
-        result = "rock";
-    } else if (rndInt == 1) {
-        result = "paper";
-    } else if (rndInt == 2) {
-        result = "scissors";
+    if (rndInt === 0) {
+        return "rock";
+    } else if (rndInt === 1) {
+        return "paper";
+    } else if (rndInt === 2) {
+        return "scissors";
     } else {
-        result = "something went wrong";
+        return "something went wrong";
     }
-    return result;
     }
 
     console.log("The computer has selected " + computerPlay() + ".");
 
+let computerSelection = computerPlay();
 
-function round(rock, paper, scissors) {
-    let result;
+function round() {
 
-    switch (computerPlay() == rock) {
-    
-        case playerSelection == rock:
-            result = "Tie game!";
-            break;
-        case playerSelection == paper:
-            result = "Paper beats rock! You win!"
-            break;
-        case playerSelection == scissors:
-            result = "Rock beats scissors! You lose!"
-            break;
-        default:
-            result = "Something went wrong."
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "Tie game!";
+        } else if (computerSelection === "paper") {
+            return "Paper beats rock! You lose!";
+        } else if (computerSelection === "scissors") {
+            return "Rock beats scissors! You win!";
+        } 
+
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "Paper beats rock! You win!";
+        } else if (computerSelection === "paper") {
+            return "Tie game!";
+        } else if (computerSelection === "scissors") {
+            return "Scissors beats paper! You lose!";
+        } 
+
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "Rock beats scissors! You lose!";
+        } else if (computerSelection === "paper") {
+            return "Scissors beats paper! You win!";
+        } else if (computerSelection === "scissors") {
+            return "Tie game!";
+        }    
+        
+        } else { 
+            return "Please input a valid option.";
+
     }
-
-    switch (computerPlay() == paper) {
-
-        case playerSelection == rock:
-            result = "Paper beats rock! You lose!"
-            break;
-        case playerSelection == paper:
-            result = "Tie game!"
-            break;
-        case playerSelection == scissors:
-            result = "Scissors beats paper! You win!"
-            break;
-        default:
-            result = "Something went wrong."
-    }
-
-    switch (computerPlay() == scissors) {
-
-        case playerSelection == rock:
-            result = "Rock beats scissors! You win!"
-            break;
-        case playerSelection == paper:
-            result = "Scissors beats paper! You lose!"
-            break;
-        case playerSelection == scissors:
-            result = "Tie game!"
-        default:
-            result = "Something went wrong."
-    }
-    return result;
     }
 
     console.log(round());
-    
+
+
+
+
 
 
 
